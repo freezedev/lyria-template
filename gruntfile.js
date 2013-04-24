a788c95e206e22f70c61c964e89e26f9b1916150
Johannes Stein <johannesstein@freeze-dev.com>
2013-04-24 22:57:16 +0200

    Updated grunt file

--- gruntfile.js
+++ gruntfile.js
@@ -6,4 +6,20 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
   
+  var createAssetArray = require('./createassetarray');
+  var prepareScenes = require('./preparescenes');
+  var path = require('path');
+
+  grunt.registerTask('build', 'Updates asset array and prepares scenes', function() {
+    var dir = './';
+    
+    createAssetArray(dir, function() {
+      prepareScenes(path.join(dir, 'assets', 'scenes'), path.join(dir, 'src', 'scenes.js'), function() {
+        grunt.log.writeln('Project built');
+      });    
+    });
+  });
+  
+  grunt.registerTask('default', 'Default task', ['build'])
+  
 };
