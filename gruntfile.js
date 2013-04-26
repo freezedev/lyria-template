f669751e9a019efec09db1d560608221b914a878
Johannes Stein <johannesstein@freeze-dev.com>
2013-04-26 23:34:47 +0200

    Updated gruntfile: Added package loading + async task

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,9 @@
 module.exports = function(grunt) {
   
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json')
+  });
+  
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -11,11 +15,13 @@ module.exports = function(grunt) {
   var path = require('path');
 
   grunt.registerTask('build', 'Updates asset array and prepares scenes', function() {
+    var done = this.async();
     var dir = './';
     
     createAssetArray(dir, function() {
       prepareScenes(path.join(dir, 'assets', 'scenes'), path.join(dir, 'src', 'scenes.js'), function() {
         grunt.log.writeln('Project built');
+        done();
       });    
     });
   });
