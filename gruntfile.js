d514067d45de38bf2f3559d4f893aa029907631b
Johannes Stein <johannesstein@freeze-dev.com>
2013-05-07 15:43:27 +0200

    Updated grunt file

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,16 @@
 module.exports = function(grunt) {
   
+  var path = require('path');
+  var fs = require('fs');
+
+  var pkgFile = require('./package.json');
+
+  var buildPrefix = 'builds';
+  var buildId = pkgFile.name + '-v' + pkgFile.version + '-' + grunt.template.today("yymmdd-HHMMss");
+  var buildFolder = path.join(buildPrefix, buildId) + path.sep;
+  
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: pkgFile
   });
   
   grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -10,19 +19,58 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
   
-  var createAssetArray = require('./createassetarray');
   var prepareScenes = require('./preparescenes');
-  var path = require('path');
 
-  grunt.registerTask('build', 'Updates asset array and prepares scenes', function() {
+  grunt.registerTask('prepare', 'Updates asset array and prepares scenes', function() {
     var done = this.async();
     var dir = './';
+    var projectName = pkgFile.name;
+    var assetObject = {};
     
-    createAssetArray(dir, function() {
-      prepareScenes(path.join(dir, 'assets', 'scenes'), path.join(dir, 'src', 'scenes.js'), function() {
-        grunt.log.writeln('Project built');
-        done();
-      });    
+    grunt.file.recurse(path.join(dir, 'assets'), function(abspath, rootdir, subdir, filename) {
+      var stat;
+      var dirname;
+      
+      if (subdir == null) {
+        subdir = '';
+      }
+      
+      if ((filename.toLowerCase().indexOf('readme') >= 0) || (subdir.indexOf('scenes') >= 0) || (subdir.indexOf('prefabs') >= 0) || (subdir.indexOf('gameobjects') >= 0)) {
+        return;
+      }
+      
+      if (!subdir) {
+        dirname = 'root';
+      } else {
+        dirname = subdir;
+      }
+      
+        assetObject[dirname] = assetObject[subdir] || {};
+        stat = fs.statSync(abspath);
+        assetObject[dirname].files = assetObject[dirname].files || [];
+        assetObject[dirname].files.push({name: abspath, size: stat.size});
+        if (assetObject[dirname].size) {
+          assetObject[dirname].totalSize += stat.size;          
+        } else {
+          assetObject[dirname].totalSize = stat.size;
+        }
+    });
+    
+    var assetSize = 0;
+    var value;
+    for (var key in assetObject) {
+      value = assetObject[key];
+      
+      assetSize += value.totalSize;
+    }
+    
+    assetObject.totalSize = assetSize;
+
+    grunt.file.write(path.join(dir, 'src', 'assetlist.js'), 'define("' + projectName + '/assetlist",' + JSON.stringify(assetObject) + ');');
+
+    prepareScenes(projectName, path.join(dir, 'assets', 'scenes'), path.join(dir, 'src', 'scenelist.js'), function() {
+      grunt.log.writeln('Project built');
+      done();
     });
   });
   
