0c4ceb46252e1832fcc1ea19ac686ebec40c6329
Johannes Stein <johannesstein@freeze-dev.com>
2013-07-02 22:43:25 +0200

    Sorry, mis-used git rm --cached

--- gruntfile.js
+++ gruntfile.js
@@ -1,24 +1,166 @@
 module.exports = function(grunt) {
-  
+
   var path = require('path');
   var fs = require('fs');
 
   var pkgFile = require('./package.json');
+  var lyriaConfig = pkgFile.lyriaProject || {};
 
   var buildPrefix = 'builds';
-  var buildId = pkgFile.name + '-v' + pkgFile.version + '-' + grunt.template.today("yymmdd-HHMMss");
+  var buildVersion = pkgFile.version + '-' + grunt.template.today("yymmdd-HHMMss");
+  var buildId = pkgFile.name + '-v' + buildVersion;
   var buildFolder = path.join(buildPrefix, buildId) + path.sep;
-  
+
+  var libFiles = fs.readdirSync('./lib');
+  var styleFiles = fs.readdirSync('./css');
+
+  var libFilesPriorities = ['almond.js', 'handlebars.runtime.js', 'fastclick.js'];
+
+  var templateScripts = [];
+  var templateStyles = [];
+
+  var uglifyLibObject = {};
+
+  for (var i = 0, j = libFiles.length; i < j; i++) {
+    (function(iterator) {
+      uglifyLibObject[path.join(buildFolder, 'lib', iterator)] = path.join('./lib', iterator);
+      if (libFilesPriorities.indexOf(iterator) >= 0) {
+        templateScripts.unshift('lib/' + iterator);
+      } else {
+        templateScripts.push('lib/' + iterator);
+      }
+    })(libFiles[i]);
+  }
+
+  uglifyLibObject[buildFolder + '/js/<%= pkg.name %>.js'] = '<%= concat.dist.dest %>';
+  templateScripts.push('js/<%= pkg.name %>.js');
+
+  for (var k = 0, l = styleFiles.length; k < l; k++) {
+    (function(iterator) {
+      templateStyles.push('css/' + iterator);
+    })(styleFiles[k]);
+  }
+
   grunt.initConfig({
-    pkg: pkgFile
+    pkg: pkgFile,
+    lyriaConfig: lyriaConfig,
+    buildVersion: buildVersion,
+    concat: {
+      dist: {
+        src: ['src/**/*.js'],
+        dest: 'js/<%= pkg.name %>.js'
+      }
+    },
+    copy: {
+      production: {
+        files: [{
+          expand: true,
+          src: ['assets/**'],
+          dest: buildFolder,
+          filter: function(filepath) {
+            if ((filepath.indexOf(path.sep + 'scenes') >= 0) || (filepath.indexOf(path.sep + 'prefabs') >= 0)) {
+              return false;
+            } else {
+              return true;
+            }
+          }
+        }, {
+          expand: true,
+          src: ['css/**'],
+          dest: buildFolder
+        }, {
+          expand: true,
+          src: ['favicon.ico'],
+          dest: buildFolder
+        }, {
+          expand: true,
+          src: ['*.png'],
+          dest: buildFolder
+        }]
+      }
+    },
+    template: {
+      development: {
+        src: 'template.html',
+        dest: 'index.html',
+        engine: 'handlebars',
+        variables: {
+          scripts: templateScripts,
+          styles: templateStyles,
+          mainModule: pkgFile.name,
+          title: '<%= lyriaConfig.title %> (Development build <%= buildVersion %>)',
+          description: lyriaConfig.description,
+          author: lyriaConfig.author
+        }
+      },
+      production: {
+        src: 'template.html',
+        dest: '<%= buildFolder %>index.html',
+        engine: 'handlebars',
+        variables: {
+          scripts: templateScripts,
+          styles: templateStyles,
+          mainModule: pkgFile.name,
+          title: lyriaConfig.title,
+          description: lyriaConfig.description,
+          author: lyriaConfig.author
+        }
+      }
+    },
+    uglify: {
+      production: {
+        files: uglifyLibObject
+      }
+    },
+    compress: {
+      deploy: {
+        options: {
+          archive: path.join(buildPrefix, buildId) + '.zip'
+        },
+        files: [{
+          cwd: buildFolder,
+          src: ['**'],
+          dest: '<%= pkg.name %>/'
+        }]
+      }
+    },
+    stylus: {
+      compile: {
+        options: {
+          paths: ['stylus'],
+          urlfunc: 'embedurl',
+          import: ['nib']
+        },
+        files: {
+          'css/<%= pkg.name %>.css': 'stylus/**/*.styl'
+        }
+      }
+    },
+    watch: {
+      stylus: {
+        files: 'stylus/**/*.styl',
+        tasks: ['stylus']
+      },
+      prepare: {
+        files: 'assets/**/*',
+        tasks: ['prepare']
+      },
+      template: {
+        files: 'template.html',
+        tasks: ['template:development']
+      }
+    }
   });
-  
+
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-templater');
+
   var prepareScenes = require('./preparescenes');
 
   grunt.registerTask('prepare', 'Updates asset array and prepares scenes', function() {
@@ -26,44 +168,47 @@ module.exports = function(grunt) {
     var dir = './';
     var projectName = pkgFile.name;
     var assetObject = {};
-    
+
     grunt.file.recurse(path.join(dir, 'assets'), function(abspath, rootdir, subdir, filename) {
       var stat;
       var dirname;
-      
+
       if (subdir == null) {
         subdir = '';
       }
-      
+
       if ((filename.toLowerCase().indexOf('readme') >= 0) || (subdir.indexOf('scenes') >= 0) || (subdir.indexOf('prefabs') >= 0) || (subdir.indexOf('gameobjects') >= 0)) {
         return;
       }
-      
+
       if (!subdir) {
         dirname = 'root';
       } else {
         dirname = subdir;
       }
-      
-        assetObject[dirname] = assetObject[subdir] || {};
-        stat = fs.statSync(abspath);
-        assetObject[dirname].files = assetObject[dirname].files || [];
-        assetObject[dirname].files.push({name: abspath, size: stat.size});
-        if (assetObject[dirname].size) {
-          assetObject[dirname].totalSize += stat.size;          
-        } else {
-          assetObject[dirname].totalSize = stat.size;
-        }
+
+      assetObject[dirname] = assetObject[subdir] || {};
+      stat = fs.statSync(abspath);
+      assetObject[dirname].files = assetObject[dirname].files || [];
+      assetObject[dirname].files.push({
+        name: abspath,
+        size: stat.size
+      });
+      if (assetObject[dirname].size) {
+        assetObject[dirname].totalSize += stat.size;
+      } else {
+        assetObject[dirname].totalSize = stat.size;
+      }
     });
-    
+
     var assetSize = 0;
     var value;
     for (var key in assetObject) {
       value = assetObject[key];
-      
+
       assetSize += value.totalSize;
     }
-    
+
     assetObject.totalSize = assetSize;
 
     grunt.file.write(path.join(dir, 'src', 'generated', 'assetlist.js'), 'define("' + projectName + '/assetlist",' + JSON.stringify(assetObject) + ');');
@@ -73,7 +218,14 @@ module.exports = function(grunt) {
       done();
     });
   });
-  
-  grunt.registerTask('default', 'Default task', ['build'])
-  
+
+  grunt.registerTask('prebuild', 'Task before building the project', ['prepare', 'concat', 'stylus']);
+
+  grunt.registerTask('development', 'Development build', ['prebuild', 'template:development']);
+  grunt.registerTask('production', 'Production build', ['prebuild', 'uglify', 'copy', 'template:production']);
+  grunt.registerTask('deploy', 'Deploys project', ['production', 'compress:deploy']);
+
+  grunt.registerTask('build', 'Builds the default project', ['development']);
+  grunt.registerTask('default', 'Default task', ['development']);
+
 };
--- src/generated/scenelist.js
+++ src/generated/scenelist.js
@@ -1,6 +1,6 @@
 define("mygame/scenelist", ["lyria/scene", "lyria/template/engine"], function(Scene, TemplateEngine) {
 	var sceneList = {};
-	sceneList["scene1"] = new Scene("scene1", function(scene) {
+	sceneList["scene1"] = new Scene("scene1", function(scene, Lyria) {
 		this.localization = {
 	"en": {
 		"title": "This is "
@@ -10,11 +10,55 @@ define("mygame/scenelist", ["lyria/scene", "lyria/template/engine"], function(Sc
 	}
 }
 ;
-		this.template = TemplateEngine.compile(function (Handlebars,depth0,helpers,partials,data) {
-  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
-helpers = helpers || Handlebars.helpers; data = data || {};
-  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
+		this.template = this.template || {};
+		this.template.source = TemplateEngine.compile(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
 
+function program1(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n  ";
+  stack1 = helpers.each.call(depth0, depth0.gameobject, {hash:{},inverse:self.noop,fn:self.program(2, program2, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n";
+  return buffer;
+  }
+function program2(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n    <div id=\"";
+  if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\">";
+  if (stack1 = helpers.content) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.content; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "</div>\n  ";
+  return buffer;
+  }
+
+function program4(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n  ";
+  stack1 = helpers.each.call(depth0, depth0.prefab, {hash:{},inverse:self.noop,fn:self.program(2, program2, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n";
+  return buffer;
+  }
+
+function program6(depth0,data) {
+  
+  var buffer = "", stack1, stack2;
+  buffer += "\n  ";
+  stack2 = ((stack1 = ((stack1 = depth0.guilayer),stack1 == null || stack1 === false ? stack1 : stack1.content)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1);
+  if(stack2 || stack2 === 0) { buffer += stack2; }
+  buffer += "\n";
+  return buffer;
+  }
 
   buffer += "<div>";
   if (stack1 = helpers.title) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
@@ -24,61 +68,115 @@ helpers = helpers || Handlebars.helpers; data = data || {};
   if (stack1 = helpers.btnSwitchToNextScene) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.btnSwitchToNextScene; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</span>";
+    + "</span>\n";
+  stack1 = helpers['if'].call(depth0, depth0.gameobject, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n\n";
+  stack1 = helpers['if'].call(depth0, depth0.prefab, {hash:{},inverse:self.noop,fn:self.program(4, program4, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n\n";
+  stack1 = helpers['if'].call(depth0, depth0.guilayer, {hash:{},inverse:self.noop,fn:self.program(6, program6, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
   return buffer;
   });
-		var sceneFunc = (function(sender, localization) {
-	
-	console.log(sender);
-	console.log(localization);
-	
-	sender.events = {
-		'#btnSwitch': {
-			'click': function(event) {
-				event.data.scene.parent.show('scene2');
-			}
-		}
-	};
-	
-	return {
-		btnSwitchToNextScene: "Switch to next scene",
-		test: "Hallo",
-		title: sender.name
-	};
-	
-})(this, this.localization);
+		var sceneFunc = (function(scene) {
+
+  scene.events = {
+    '#btnSwitch': {
+      'click': function(event) {
+        event.data.scene.parent.show('scene2');
+      }
+    }
+  };
+
+  scene.expose({
+    btnSwitchToNextScene: "Switch to next scene",
+    test: "Hallo",
+    title: scene.name
+  });
+
+})(this);
 ;
-		if (typeof sceneFunc === "function") { sceneFunc = sceneFunc.apply(scene, scene); }		return sceneFunc;	});
-	sceneList["scene2"] = new Scene("scene2", function(scene) {
+		if (typeof sceneFunc === "function") { sceneFunc = sceneFunc.apply(scene, [scene, Lyria]); }		return sceneFunc;	});
+	sceneList["scene2"] = new Scene("scene2", function(scene, Lyria) {
 		this.localization = {
 	"en": {},
 	"de": {}
 }
 ;
-		this.template = TemplateEngine.compile(function (Handlebars,depth0,helpers,partials,data) {
-  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
-helpers = helpers || Handlebars.helpers; data = data || {};
-  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
+		this.template = this.template || {};
+		this.template.source = TemplateEngine.compile(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
 
+function program1(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n  ";
+  stack1 = helpers.each.call(depth0, depth0.gameobject, {hash:{},inverse:self.noop,fn:self.program(2, program2, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n";
+  return buffer;
+  }
+function program2(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n    <div id=\"";
+  if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\">";
+  if (stack1 = helpers.content) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.content; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "</div>\n  ";
+  return buffer;
+  }
+
+function program4(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n  ";
+  stack1 = helpers.each.call(depth0, depth0.prefab, {hash:{},inverse:self.noop,fn:self.program(2, program2, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n";
+  return buffer;
+  }
+
+function program6(depth0,data) {
+  
+  var buffer = "", stack1, stack2;
+  buffer += "\n  ";
+  stack2 = ((stack1 = ((stack1 = depth0.guilayer),stack1 == null || stack1 === false ? stack1 : stack1.content)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1);
+  if(stack2 || stack2 === 0) { buffer += stack2; }
+  buffer += "\n";
+  return buffer;
+  }
 
   buffer += "<div>";
   if (stack1 = helpers.test) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.test; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</div>";
+    + "</div>\n";
+  stack1 = helpers['if'].call(depth0, depth0.gameobject, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n\n";
+  stack1 = helpers['if'].call(depth0, depth0.prefab, {hash:{},inverse:self.noop,fn:self.program(4, program4, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n\n";
+  stack1 = helpers['if'].call(depth0, depth0.guilayer, {hash:{},inverse:self.noop,fn:self.program(6, program6, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
   return buffer;
   });
-		var sceneFunc = (function(sender, localization) {
-	
-	console.log(sender);
-	console.log(localization);
-	
-	return {
-		test: "Hallo"
-	};
-	
-})(this, this.localization);
+		var sceneFunc = (function(scene) {
+
+  scene.expose({
+    test: 'Hallo'
+  });
+
+})(this);
 ;
-		if (typeof sceneFunc === "function") { sceneFunc = sceneFunc.apply(scene, scene); }		return sceneFunc;	});
+		if (typeof sceneFunc === "function") { sceneFunc = sceneFunc.apply(scene, [scene, Lyria]); }		return sceneFunc;	});
 	return sceneList;
 });
