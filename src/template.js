841de025ec305ff2dc08326b40408d117c56ce93
Johannes Stein <johannesstein@freeze-dev.com>
2013-07-02 22:40:55 +0200

    Updated main file

--- src/template.js
+++ src/template.js
@@ -1,10 +1,19 @@
-define('mygame', ['lyria/game', 'mygame/scenelist', 'mygame/assetlist'], function(Game) {
+define('mygame', ['lyria/game', 'mygame/scenelist', 'mygame/assetlist'], function(Game, sceneList, assetList) {
   'use strict';
   
 	var myGame = new Game();
 	
-	myGame.director.add('scene1');
-	myGame.director.add('scene2');
+	myGame.director.precompiledScenes = sceneList;
 	
-	myGame.director.show('scene1');
+	myGame.preloader.on('complete', function() {
+    myGame.director.add('scene1');
+    myGame.director.add('scene2');
+
+    myGame.director.show('scene1'); 
+  });
+  
+  myGame.preloader.assets = assetList;
+  myGame.preloader.start();
+	
+	
 });
