65455caeaa7a447fc960e9133beb7b5449a44852
Johannes Stein <johannesstein@freeze-dev.com>
2013-07-23 22:50:43 +0200

    Comment parsing for getting scene dependencies and scene options

--- preparescenes.js
+++ preparescenes.js
@@ -3,6 +3,12 @@ var path = require('path');
 var handlebars = require('handlebars');
 var UglifyJS = require('uglify-js');
 
+// Using esprima might be overkill, but maybe I could use it for other stuff?
+var esprima = require('esprima');
+
+// TODO: Add sourcemaps
+// TODO: Make this available for prefabs as well
+
 var defaultSceneTemplate = "{{#if gameobject}}\n  {{#each gameobject}}\n    <div id=\"{{name}}\">{{{content}}}</div>\n  {{/each}}\n{{/if}}\n\n{{#if prefab}}\n  {{#each prefab}}\n    <div id=\"{{name}}\">{{{content}}}</div>\n  {{/each}}\n{{/if}}\n\n{{#if guilayer}}\n  {{{guilayer.content}}}\n{{/if}}";
 
 module.exports = function(namespace, scenePath, output, callback) {
@@ -17,14 +23,65 @@ module.exports = function(namespace, scenePath, output, callback) {
     if (path.extname(scene) === '') {
       if (fs.existsSync(path.join(scenePath, scene))) {
         
-        sceneObject[scene] = {};
-        
-        sceneObject += '\tsceneList["' + scene + '"] = new Scene("' + scene + '", function(scene, Lyria) {\n';
-        
         var sceneFunc = path.join(scenePath, scene, 'scene.js');
+        var sceneFuncContent = fs.existsSync(sceneFunc) ? fs.readFileSync(sceneFunc, 'utf8') : null;
         var sceneLoc = path.join(scenePath, scene, 'localization.json');
         var sceneMarkup = path.join(scenePath, scene, 'scene.html');
         var scenePartials = path.join(scenePath, scene, 'partials');
+        
+        var parsedSceneFunc = {};
+        
+        var sceneDeps = '[]';
+        var options = '{}';
+        
+        
+        if (sceneFuncContent != null) {
+          parsedSceneFunc = esprima.parse(sceneFuncContent, {comment: true});
+          
+          var commentArray = parsedSceneFunc.comments;
+          console.log(commentArray);
+          
+          if (commentArray != null && Array.isArray(commentArray) && commentArray.length > 0) {
+            
+            for (var i = 0, j = commentArray.length; i < j; i++) {
+              
+              (function(comment) {
+                if ((comment.type == null && comment.value == null) || comment.type !== 'Block') {
+                  return;
+                }
+                
+                var value = comment.value;
+                console.log(value);
+                
+                console.log(comment);
+                if (value.indexOf(':') > 0) {
+                  var splitted = value.split(':');
+            
+                  var name = splitted[0];
+                  var data = splitted[1];
+                  
+                  switch(name) {
+                    case 'depends':
+                      sceneDeps = data;
+                      console.log(sceneDeps);
+                      break;
+                      
+                    case 'options':
+                      options = data;
+                    
+                    default:
+                      break;
+                  }
+                }
+              })(commentArray[i]);
+            }          
+          }
+        }
+        
+        
+        sceneObject[scene] = {};
+        
+        sceneObject += '\tsceneList["' + scene + '"] = new Scene("' + scene + '", ' + sceneDeps + ', function() {\n';
         
         if (fs.existsSync(sceneLoc)) {
           try {
@@ -51,9 +108,9 @@ module.exports = function(namespace, scenePath, output, callback) {
         }
         
         
-        if (fs.existsSync(sceneFunc)) {
-          sceneObject += '\t\tvar sceneFunc = ' + fs.readFileSync(sceneFunc, 'utf8') + ';\n';
-          sceneObject += '\t\tif (typeof sceneFunc === "function") { sceneFunc = sceneFunc.apply(scene, [scene, Lyria]); }';
+        if (sceneFuncContent != null) {
+          sceneObject += '\t\tvar sceneFunc = ' + sceneFuncContent + ';\n';
+          sceneObject += '\t\tif (typeof sceneFunc === "function") { sceneFunc = sceneFunc.apply(this, arguments); }';
           sceneObject += '\t\treturn sceneFunc;';
         }
         
