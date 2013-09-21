55398ac9c4d6e1d2f23fbfcb851cfe803531eb49
Johannes Stein <johannesstein@freeze-dev.com>
2013-09-21 23:48:26 +0200

    Updated generated files

--- src/generated/scenelist.js
+++ src/generated/scenelist.js
@@ -3,18 +3,21 @@ define("mygame/scenelist", ["lyria/scene", "lyria/template/engine"], function(Sc
 	sceneList["scene1"] = new Scene("scene1", [], function() {
 		this.localization = {
 	"en": {
-		"title": "This is "
+		"title": "This is {{name}}",
+		"btnSwitchToNextScene": "Switch to next scene"
 	},
 	"de": {
-		"title": "Das ist "
+		"title": "Das ist {{name}}",
+		"btnSwitchToNextScene": "Zur nächsten Szene wechseln"
 	}
 }
 ;
 		this.template = this.template || {};
-		this.template.source = TemplateEngine.compile(function (Handlebars,depth0,helpers,partials,data) {
+		var self = this
+;		this.template.helpers["translate"] = this.t;		this.template.source = TemplateEngine.compile(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
+  var buffer = "", stack1, stack2, options, functionType="function", escapeExpression=this.escapeExpression, self=this, helperMissing=helpers.helperMissing;
 
 function program1(depth0,data) {
   
@@ -65,37 +68,39 @@ function program6(depth0,data) {
   else { stack1 = depth0.title; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
     + "</div>\r\n\r\n<span id=\"btnSwitch\">";
-  if (stack1 = helpers.btnSwitchToNextScene) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = depth0.btnSwitchToNextScene; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
-  buffer += escapeExpression(stack1)
+  options = {hash:{},data:data};
+  buffer += escapeExpression(((stack1 = helpers.translate || depth0.translate),stack1 ? stack1.call(depth0, "btnSwitchToNextScene", options) : helperMissing.call(depth0, "translate", "btnSwitchToNextScene", options)))
     + "</span>\n";
-  stack1 = helpers['if'].call(depth0, depth0.gameobject, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
+  stack2 = helpers['if'].call(depth0, depth0.gameobject, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack2 || stack2 === 0) { buffer += stack2; }
   buffer += "\n\n";
-  stack1 = helpers['if'].call(depth0, depth0.prefab, {hash:{},inverse:self.noop,fn:self.program(4, program4, data),data:data});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
+  stack2 = helpers['if'].call(depth0, depth0.prefab, {hash:{},inverse:self.noop,fn:self.program(4, program4, data),data:data});
+  if(stack2 || stack2 === 0) { buffer += stack2; }
   buffer += "\n\n";
-  stack1 = helpers['if'].call(depth0, depth0.guilayer, {hash:{},inverse:self.noop,fn:self.program(6, program6, data),data:data});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
+  stack2 = helpers['if'].call(depth0, depth0.guilayer, {hash:{},inverse:self.noop,fn:self.program(6, program6, data),data:data});
+  if(stack2 || stack2 === 0) { buffer += stack2; }
   return buffer;
-  });
-		var sceneFunc = (function(scene) {
+  }, {partials: self.template.partials, helpers: self.template.helpers});
+		var sceneFunc = (function(scene, Lyria) {
 
   scene.events = {
     '#btnSwitch': {
       'click': function(event) {
-        event.data.scene.parent.show('scene2');
+        scene.parent.show('scene2');
       }
     }
   };
 
   scene.expose({
-    btnSwitchToNextScene: "Switch to next scene",
     test: "Hallo",
-    title: scene.name
+    title: scene.t('title', {
+      name: scene.name
+    })
   });
+  
+  scene.log('yeeha!');
 
-})(this);
+})(this, arguments[1]);
 ;
 		if (typeof sceneFunc === "function") { sceneFunc = sceneFunc.apply(this, arguments); }		return sceneFunc;	});
 	sceneList["scene2"] = new Scene("scene2", [], function() {
@@ -105,7 +110,8 @@ function program6(depth0,data) {
 }
 ;
 		this.template = this.template || {};
-		this.template.source = TemplateEngine.compile(function (Handlebars,depth0,helpers,partials,data) {
+		var self = this
+;		this.template.helpers["translate"] = this.t;		this.template.source = TemplateEngine.compile(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
@@ -168,13 +174,13 @@ function program6(depth0,data) {
   stack1 = helpers['if'].call(depth0, depth0.guilayer, {hash:{},inverse:self.noop,fn:self.program(6, program6, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   return buffer;
-  });
+  }, {partials: self.template.partials, helpers: self.template.helpers});
 		var sceneFunc = (function(scene) {
-
+  
   scene.expose({
     test: 'Hallo'
   });
-
+  
 })(this);
 ;
 		if (typeof sceneFunc === "function") { sceneFunc = sceneFunc.apply(this, arguments); }		return sceneFunc;	});
