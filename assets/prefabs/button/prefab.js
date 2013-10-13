0515e030db427d1707814a36687ea5ca4b1bb60b
Johannes Stein <johannesstein@freeze-dev.com>
2013-10-13 19:50:52 +0200

    Added prefab

---
+++ assets/prefabs/button/prefab.js
@@ -0,0 +1,29 @@
+(function(scene) {
+  
+  var Lyria = scene.modules.Lyria;
+
+  scene.on('achievement', function() {
+    Lyria.AchievementManager.show('switchScene');
+  });
+
+  scene.bindEvents({
+    '#btnSwitch': {
+      'click': function(event) {
+        scene.trigger('achievement');
+        scene.parent.show('scene2');
+      }
+    }
+  });
+
+  scene.expose({
+    test: "Hallo",
+    title: scene.t('title', {
+      name: scene.name
+    })
+  });
+
+  console.log(scene);
+  console.log(scene.game);
+  scene.log('yeeha!');
+
+})(this);
